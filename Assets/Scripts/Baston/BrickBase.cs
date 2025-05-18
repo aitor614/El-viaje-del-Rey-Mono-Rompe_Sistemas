1d15cc474e07ee02a2e3ccc232b45835aa04f578
aitor614 <110167746+aitor614@users.noreply.github.com>
2025-05-18 13:24:15 +0200

    Añadir bricks con golpes

--- Assets/Scripts/Baston/BrickBase.cs
+++ Assets/Scripts/Baston/BrickBase.cs
@@ -11,12 +11,20 @@ public class BrickBase : MonoBehaviour
     public AudioClip sonidoDestruccion;
     public AudioClip sonidoToque;
 
+    [Header("Sprites")]
+    public Sprite[] spritesPorEstado;
+
     private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
 
     private void Start()
     {
         control = ControlBreakout.InstanciaControl;
         audioSource = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (spritesPorEstado.Length > 0)
+            spriteRenderer.sprite = spritesPorEstado[0];
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -25,15 +33,21 @@ public class BrickBase : MonoBehaviour
 
         vidas--;
 
-        // Reproducir sonido de toque
-        if (vidas > 0 && sonidoToque != null) audioSource.PlayOneShot(sonidoToque);
+        if (vidas > 0)
+        {
+            // Reproducir sonido de toque
+            if (sonidoToque != null)
+                audioSource.PlayOneShot(sonidoToque);
 
-        if (vidas <= 0)
+            // Si hay un sprite disponible para este nivel de daño, lo cambiamos
+            int indexSprite = Mathf.Clamp(spritesPorEstado.Length - vidas, 0, spritesPorEstado.Length - 1);
+            if (spritesPorEstado.Length > 1 && spritesPorEstado[indexSprite] != null)
+                spriteRenderer.sprite = spritesPorEstado[indexSprite];
+        }
+        else
         {
             if (sonidoDestruccion != null)
-            {
                 AudioSource.PlayClipAtPoint(sonidoDestruccion, transform.position);
-            }
 
             // Sumar puntos y ladrillos
             PlayerPrefs.SetInt("Ladrillos", PlayerPrefs.GetInt("Ladrillos") + 1);
