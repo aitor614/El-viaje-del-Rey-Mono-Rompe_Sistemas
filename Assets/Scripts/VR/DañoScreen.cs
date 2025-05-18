c4f4ad27a2072a4adbdaae8a5e2c3d2bb08e4a6f
BeaLP91 <bea1991@uoc.edu>
2025-05-18 19:10:42 +0200

    pantalla pausa vr

--- Assets/Scripts/VR/DañoScreen.cs
+++ Assets/Scripts/VR/DañoScreen.cs
@@ -4,10 +4,20 @@ using UnityEngine.UI;
 public class DañoScreen : MonoBehaviour
 {
     public Image imagenDaño; 
-    public float duracion = 1f;
+    public float duracion = 0.5f;
+    public AudioClip sonidoDaño; // arrastrar desde el Inspector
+    private AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void MostrarDaño()
     {
+        if (sonidoDaño != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(sonidoDaño);
+        }
         StopAllCoroutines(); 
         StartCoroutine(FlashDaño());
     }
